5d430c2be13acf6f3e88cdaae0a2cb2ba186e337
Skowiz <aubourgadrien@gmail.com>
2021-11-29 14:11:53 +0100

    Use our fork of openzeppelin
    We need _allowances goes public for overwrite the transferFrom function for taxes.
    That is why we needed to fork openzeppelin contracts

--- XMAS.sol
+++ XMAS.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.8.4;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/Crazy-Santa/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 
 contract XMAS is ERC20{
